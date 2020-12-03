c1bd9ba0e5b39c284e2a91cd1f9aa7c4c5d092dd
Alisa Poon <alisa.poon97@gmail.com>
2020-12-02 21:46:32 -0800

    catch error if no search results found

--- client/src/hooks/useBooks.js
+++ client/src/hooks/useBooks.js
@@ -11,26 +11,35 @@ const useBooks = (defaultSearchTerm) => {
   }, [defaultSearchTerm]);
 
   const search = async term => {
-    const response = await googleBooks.get("/", {
-      params: {
-        q: term,
-        maxResults: 5,
-      }
-    });
-    const booksArray = []
-    response.data.items.forEach(item => {
-      const book = {}
-      const {title, description, infoLink, authors, imageLinks} = item.volumeInfo;
-      book.title = title;
-      book.authors = authors;
-      book.description = description;
-      book.image = imageLinks.thumbnail;
-      book.link = infoLink;
-      console.log(item.volumeInfo);
-      booksArray.push(book);
-    })
-    console.log(booksArray)
-    setBooks(response.data.items)
+    try {
+      const response = await googleBooks.get("/", {
+        params: {
+          q: term,
+          maxResults: 5,
+        }
+      });
+      const booksArray = []
+      response.data.items.forEach(item => {
+        const book = {}
+        const {title, description, infoLink, authors, imageLinks} = item.volumeInfo;
+        book.title = title;
+        book.authors = authors;
+        book.description = description;
+        book.image = imageLinks.thumbnail;
+        book.link = infoLink;
+        booksArray.push(book);
+      })
+      console.log(booksArray)
+      setBooks(response.data.items)
+    } catch (e) {
+      console.log(`Your search - ${term} - did not match any book results. 
+      
+Suggestions: 
+      * Make sure all words are spelled correctly.
+      * Try different keywords.
+      * Try more general keywords.
+      * Try fewer keywords.`)
+    }
   };
 
   // title: String,
